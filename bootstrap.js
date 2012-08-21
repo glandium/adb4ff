bc206b51f573827848f38b37a9ddd2ce6d928f8e
Mike Hommey <mh@glandium.org>
2012-08-21 16:42:34 +0200

    Implement a fully fledged nsIChannel instead of using nsIInputStreamChannel

--- bootstrap.js
+++ bootstrap.js
@@ -8,6 +8,7 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cm = Components.manager;
 const Cu = Components.utils;
+const Cr = Components.results;
 
 Cm.QueryInterface(Ci.nsIComponentRegistrar);
 
@@ -16,28 +17,132 @@ Cu.import('resource://gre/modules/Services.jsm');
 
 var baseURL = null;
 
-function newADBDeviceListChannel(uri)
-{
-  var channel = Cc['@mozilla.org/network/input-stream-channel;1'].createInstance(Ci.nsIInputStreamChannel);
-  var pipe = Cc['@mozilla.org/pipe;1'].createInstance(Ci.nsIPipe);
-  pipe.init(true, false, 0, 0, null);
-  channel.setURI(uri);
-  channel.contentStream = pipe.inputStream;
-  channel.QueryInterface(Ci.nsIChannel);
-  channel.contentType = 'application/http-index-format';
-  ADB.devices(function(devices) {
-  var data = '300: ' + uri.scheme + ':///\n' +
-             '200: filename content-length last-modified file-type\n';
-    for each (var d in devices) {
-      if (d.status != 'offline')
-        data += '201: ' + d.serial + ' 0 Thu,%201%20Jan%201970%2000:00:00%20GMT DIRECTORY\n';
-    }
-
-    pipe.outputStream.write(data, data.length);
-    pipe.outputStream.close();
-  });
-  return channel;
+/* Parts of the channel implementation are stolen from
+ * browser/components/thumbnails/PageThumbsProtocol.js */
+function ADBChannel(uri) {
+  this._uri = uri;
+  this.originalURI = uri;
 }
+
+ADBChannel.prototype = {
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIChannel, Ci.nsIRequest, Ci.nsIStreamListener]),
+  /* nsIChannel */
+  owner: null,
+  notificationCallbacks: null,
+  get securityInfo() null,
+  contentType: null,
+  contentCharset: null,
+  contentLength: -1,
+  get URI() this._uri,
+
+  open: function ADBChannel_open() {
+    throw (Components.returnCode = Cr.NS_ERROR_NOT_IMPLEMENTED);
+  },
+
+  asyncOpen: function ADBChannel_asyncOpen(listener, context) {
+    if (this._isPending)
+      throw (Components.returnCode = Cr.NS_ERROR_IN_PROGRESS);
+
+    if (this._wasOpened)
+      throw (Components.returnCode = Cr.NS_ERROR_ALREADY_OPENED);
+
+    if (this._canceled)
+      return (Components.returnCode = this._status);
+
+    if (this._uri.scheme == 'adbview' && this._uri.path != '/')
+      throw (Components.returnCode = Cr.NS_ERROR_FILE_NOT_FOUND);
+
+    this._isPending = true;
+    this._wasOpened = true;
+
+    this._listener = listener;
+    this._context = context;
+
+    if (this.loadGroup)
+      this.loadGroup.addRequest(this, null);
+
+    if (this._canceled)
+      return;
+
+    var that = this;
+
+    if (!this._uri.host)
+      ADB.devices(function(devices) {
+        that.contentType = 'application/http-index-format';
+        var data = '300: ' + that._uri.scheme + ':///\n' +
+                   '200: filename content-length last-modified file-type\n';
+        for each (var d in devices) {
+          if (d.status != 'offline')
+            data += '201: ' + d.serial + ' 0 Thu,%201%20Jan%201970%2000:00:00%20GMT DIRECTORY\n';
+        }
+
+        var stream = Cc["@mozilla.org/io/string-input-stream;1"].createInstance(Ci.nsIStringInputStream);
+        stream.setData(data, data.length);
+        that._pump = Cc["@mozilla.org/network/input-stream-pump;1"].createInstance(Ci.nsIInputStreamPump);
+        that._pump.init(stream, -1, -1, 0, 0, true);
+        that._pump.asyncRead(that, null);
+      });
+    else
+      that.onStopRequest(that, null, Cr.NS_ERROR_FILE_NOT_FOUND);
+  },
+
+  /* nsIRequest */
+  _status: Cr.NS_OK,
+  _canceled: false,
+  _isPending: false,
+
+  get status() this._status,
+  get name() this._uri.spec,
+  loadFlags: Ci.nsIRequest.LOAD_NORMAL,
+  loadGroup: null,
+
+  isPending: function ADBChannel_isPending() this._isPending,
+
+  cancel: function ADBChannel_cancel(status) {
+    if (this._canceled)
+      return;
+
+    this._canceled = true;
+    this._status = status;
+
+    if (this._pump)
+      this._pump.cancel(status);
+  },
+
+  suspend: function ADBChannel_suspend() {
+    if (this._pump)
+      this._pump.suspend();
+  },
+
+  resume: function ADBChannel_resume() {
+    if (this._pump)
+      this._pump.resume();
+  },
+
+  /* nsIStreamListener */
+  onStartRequest: function ADBChannel_onStartRequest(request, context) {
+    if (!this.canceled && Components.isSuccessCode(this._status))
+      this._status = request.status;
+
+    this._listener.onStartRequest(this, this._context);
+  },
+
+  onDataAvailable: function ADBChannel_onDataAvailable(request, context, instream, offset, count) {
+    this._listener.onDataAvailable(this, this._context, instream, offset, count);
+  },
+
+  onStopRequest: function ADBChannel_onStopRequest(request, context, status) {
+    this._isPending = false;
+    this._status = status;
+
+    this._listener.onStopRequest(this, this._context, status);
+    this._listener = null;
+    this._context = null;
+
+    if (this.loadGroup)
+      this.loadGroup.removeRequest(this, null, status);
+  },
+};
 
 function ADBProtocolHandler() {}
 
@@ -61,7 +166,7 @@ ADBProtocolHandler.prototype = {
   newChannel: function ADBProtocolHandler_newChannel(uri)
   {
     if (!uri.host)
-      return newADBDeviceListChannel(uri);
+      return new ADBChannel(uri);
 
     var channel = Cc['@mozilla.org/network/input-stream-channel;1'].createInstance(Ci.nsIInputStreamChannel);
     var pipe = Cc['@mozilla.org/pipe;1'].createInstance(Ci.nsIPipe);
@@ -129,7 +234,7 @@ ADBViewProtocolHandler.prototype = {
   newChannel: function ADBProtocolHandler_newChannel(uri)
   {
     if (!uri.host)
-      return newADBDeviceListChannel(uri);
+      return new ADBChannel(uri);
     var path;
     if (uri.path == '/') {
       path = baseURL + '/framebuffer.html';
